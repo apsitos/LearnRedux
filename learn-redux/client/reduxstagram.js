44ba90cf84b9cd387543a93616ef69ffa8705c67
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-01-17 13:51:32 -0700

    first commit

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,21 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import { Link } from 'react-router';
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/Photogrid';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+//Import CSS
+import css from './styles/style.styl';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'))
