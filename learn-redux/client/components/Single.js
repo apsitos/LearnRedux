a9f8ba98a97c87d30e9a5e2777b7217c8941914b
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-01-17 16:16:32 -0700

    through video 16

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Photo from './Photo';
+import Comment from './Comments'
+
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
     return(
       <div className="single-photo">
-        I'm the single.
+        <Photo i={i} post={post} {...this.props} />
+        <Comment postComments={postComments} {...this.props} />
       </div>
     )
   }
